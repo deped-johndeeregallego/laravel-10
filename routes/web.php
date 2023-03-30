1dd336191e1fb91a6e4f216c789b5c700b4c740d
unknown <johndeere.gallego@deped.gov.ph>
2023-03-30 14:25:01 +0800

    add some stuff

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +17,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+   
+    // $users = DB::select ("SELECT * from users where id='1'");
+    // $users = DB::insert('insert into users(name, email, password) values (?, ?, ?)',['John', 'sample@gmail.com', 'password']);
+    //  $users = DB::update("update users set email=? WHERE id=?", ['123@gmail.com',1]);
+    // $users = DB:: delete("delete from users WHERE id=?", [1]);
+
+    $users = DB::table('users')->get();
+    
+    // $users = DB::table('users')->where('id', 2)->get();
+    // $users = User::create([
+    //     'name' => 'new_johnq_pampers',
+    //     'email' => '1233_john_pampers@gmail.com',
+    //     'password' => '123456'
+    // ]);
+
+    // $users = DB::table('users')->where('id' , 3)->update(['email' => 'pampers1@gmail.com']);
+    // $users = DB::table('users')->where('id', 3)->delete();
+     dd($users);
 });
 
 Route::get('/dashboard', function () {
